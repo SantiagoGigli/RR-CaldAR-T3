9b580f35b7d033fa34d47317a384e2ad3ad38687
Santiago Gigli <santiago.gigli@radiumrocket.com>
2020-11-21 20:54:16 -0300

    getCustomerById added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const customers = require('./data/Customers.json')
+
 app.get('/', (req, res) => {
   res.send('Hello world!')
 })
@@ -9,3 +11,24 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+//get all customers
+
+getAllCustomers = () =>{
+  app.get('/customers', (req, res) => {
+    res.json(customers);
+  })
+}
+
+getAllCustomers();
+
+//get customers by id
+
+getCustomerById = () => {
+  app.get('/customers/:id', (req,res) => {
+    res.json(customers.filter(tech => tech.id === parseInt(req.params.id)))
+  })
+}
+
+getCustomerById();
