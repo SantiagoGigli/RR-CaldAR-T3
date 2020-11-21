9ac2f0e99f99548ec9ca6141ad316ec7620925a8
Augusto <augustoarlt95@gmail.com>
2020-11-21 20:04:45 -0300

    Add get building by id endpoint

--- controllers/buildings.js
+++ controllers/buildings.js
@@ -1,9 +1,16 @@
+const { json } = require('express');
 const express = require('express');
 const router = express.Router();
 const buildingsData = require('../data/BULDINGS_MOCK_DATA');
 
 router.get('/getAllBuildings', (req, res) => {
     res.json(buildingsData)
+});
+
+router.get('/getById/:id', (req, res) => {
+    const requestBuildingId = parseInt(req.params.id);
+    const building = buildingsData.filter((building) => building.id === requestBuildingId);
+    building.length ? res.json(building) : res.status(400).json({msg: `The building with id ${requestBuildingId} doesn't exist`});
 });
 
 module.exports = router;
