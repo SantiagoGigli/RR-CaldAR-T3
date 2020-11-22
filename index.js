87a774883f629859996b39b68d6a4bbd82e2b695
Ignacio Aristo <ignacioaristo@gmail.com>
2020-11-21 23:01:30 -0300

    function TechniciansByAttribute done

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+const express = require('express');
+const app = express();
+const port = 3000;
 const technicians = require('./data/technicians.json');
+// const fs = require('fs');
+// const data = fs.readFileSync('./data/technicians.json');
+// const techniciansData = JSON.parse(data);
+
 
 
 app.get('/', (req, res) => {
@@ -13,14 +16,12 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-
 // Get all the technicians
 getTechniciansAll = () => {
   app.get('/technicians', (req,res)=>{
     res.json(technicians);
   })
 }
-
 getTechniciansAll();
 
 // Get technicians by ID
@@ -30,14 +31,16 @@ getTechnicianById = () => {
     res.json(technicians.filter(tech => tech.id === parseInt(req.params.id)))
   })
 }
-
 getTechnicianById();
 
-// Get technicians by attribute
+// Get technicians by attribute (Name)
 
-
-
-
+TechniciansByAttribute = () => {
+  app.get('/technicians/firstname/:first_name', (req,res)=>{
+    res.json(technicians.filter(tech => tech.first_name.includes(req.params.first_name)))
+  })
+}
+TechniciansByAttribute();
 
 // Delete technicians by ID
 
@@ -45,6 +48,6 @@ deleteTechnicianById = () => {
   app.delete('/technicians/:id', (req,res) => {
     res.json({msg: `Technician ${req.params.id} deleted`, technicians: technicians.filter(tech => tech.id !== parseInt(req.params.id))})
   })
-}
 
+}
 deleteTechnicianById();
