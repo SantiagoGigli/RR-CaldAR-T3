c4cc8f534a5662aa6b8f78883eea4219ff4318f4
Emiliano Barro <emilianobarro8@gmail.com>
2020-11-23 01:27:12 -0300

    create getAllBoilersTypes

--- controllers/boilers-types.js
+++ controllers/boilers-types.js
@@ -7,7 +7,8 @@ const fs= require('fs');
 // Get list boiler types by attribute type
 // http://localhost:3000/getBoilersTypesByType/D
 router.get('/getBoilersTypesByType/:type', (req,res)=>{
-    console.log('llega2');
+    
+    console.log('getBoilersTypesByType:'+req.params.type);
 
     const found=boilers_types.some(boilers_types=>boilers_types.description.substring(5,6)==req.params.type)
     
@@ -18,6 +19,15 @@ router.get('/getBoilersTypesByType/:type', (req,res)=>{
         res.status(400).json({msg: 'There are no boilers of the types ' + req.params.type });
     }
 });
+
+
+// Get list boiler types
+// http://localhost:3000/getAllBoilersTypes
+router.get('/getAllBoilersTypes',(req,res)=>{
+    console.log('getAllBoilersTypes');
+    res.json(boilers_types);
+});
+
 
 
 module.exports=router;
