ad7f6850f875efd49751decd69a855cda536c668
SantiagoGigli <santigigli93@gmail.com>
2020-11-23 17:46:30 -0300

    query params added, indentation and spacing improved

---
+++ controllers/customers.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const fs = require('fs');
+const router = express.Router();
+const customers = require('../data/Customers.json');
+
+//get all customers
+router.get('/customers', (req, res) => {
+  res.json(customers);
+})
+
+//get customers by id
+router.get('/getCustomersById', (req,res) => {
+  const id = req.query.id;
+  const customer = customers.find(customer => customer.id === parseInt(id));
+  res.json(customer);
+})
+
+//get customers by name
+router.get('/getCustomersByType', (req,res) => {
+  const type = req.query.customerType;
+  const ctype = customers.filter(customer => customer.customerType.includes(type));
+  res.json(ctype);
+})
+
+//delete customer by id
+router.get('/deleteCustomerById', (req,res) => {
+  const id = req.query.id;
+  const customerD = customers.find(customer => customer.id === parseInt(id));
+  const remCustomers = customers.filter(customer => customer.id !== parseInt(customerD));
+  fs.writeFile('data/Customers.json', JSON.stringify(remCustomers), err => {
+    if(err){
+      console.log(err);
+    }
+  });
+  res.json({msg: 'Customer deleted ', Customers: remCustomers});
+})
+
+module.exports = router;
