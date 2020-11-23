e147c54848a5221d3368dde40e45f6b7f3ccd30e
Augusto <augustoarlt95@gmail.com>
2020-11-22 23:02:36 -0300

    refactor building controller

--- controllers/buildings.js
+++ controllers/buildings.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
+const abmUtils = require('../utils/ABMUtils');
 const buildingsData = require('../data/BULDINGS_MOCK_DATA.json');
 
 router.get('/getAllBuildings', (req, res) => {
@@ -8,39 +8,15 @@ router.get('/getAllBuildings', (req, res) => {
 });
 
 router.get('/getById/:id', (req, res) => {
-    const requestBuildingId = parseInt(req.params.id);
-    const building = buildingsData.filter(building => building.id === requestBuildingId);
-    building.length ? res.json(building) : res.status(400).json({msg: `The building with id ${requestBuildingId} doesn't exist`});
+    abmUtils.getById(req, res, buildingsData);
 });
 
 router.get('/getByAttribute', (req, res) => {
-    const queryKeys = Object.keys(req.query);
-    const queryAttr = queryKeys.map(key => req.query[key]);
-    const matchingBuildings = [];
-    for (let i = 0; i < buildingsData.length; i++) {
-        const building = buildingsData[i];
-        const buildingAttr = queryKeys.map(key => building[key]);
-        if (queryAttr.length === buildingAttr.length && buildingAttr.every((val, index) => val == queryAttr[index])) {
-            matchingBuildings.push(building);
-        }
-    }
-    matchingBuildings.length ? res.json(matchingBuildings) : res.status(400).json({msg: 'There isn\'t any building match with your search'})
+    abmUtils.getByAttribute(req, res, buildingsData);
 });
 
 router.delete('/deleteById/:id', (req, res) => {
-    const requestBuildingId = parseInt(req.params.id);
-    const building = buildingsData.some(building => building.id === requestBuildingId);
-    if (building) {
-        const newData = buildingsData.filter(building => building.id !== requestBuildingId);
-        fs.writeFile(__dirname + '/../data/BULDINGS_MOCK_DATA.json', JSON.stringify(newData), err => {
-            if (err) {
-                console.log(err);
-            }
-        })
-        res.json({msg: 'Building deleted', buildings: newData});
-    } else {
-        res.status(400).json({msg: `The building with id ${requestBuildingId} doesn't exist`});
-    }
+    abmUtils.deleteById(req, res, buildingsData, '/../data/BULDINGS_MOCK_DATA.json');
 });
 
 module.exports = router;
