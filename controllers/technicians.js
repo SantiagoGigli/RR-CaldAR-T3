25234ce067ab6499ec542cc500f9f5c627468801
Ignacio Aristo <ignacioaristo@gmail.com>
2020-11-23 13:37:20 -0300

    delete function now delete the database

--- controllers/technicians.js
+++ controllers/technicians.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const technicians = require('../data/technicians.json');
+const fs = require('fs');
 
 // Get all the technicians
 
@@ -14,28 +15,35 @@ getTechniciansAll = () => {
 // Get technicians by ID
 
 getTechnicianById = () => {
-app.get('/technicians/:id', (req,res) => {
-    res.json(technicians.filter(tech => tech.id === parseInt(req.params.id)))
-})
+    app.get('/technicians/:id', (req,res) => {
+        res.json(technicians.filter(tech => tech.id === parseInt(req.params.id)));
+    })
 }
 getTechnicianById();
 
 // Get technicians by attribute (Name)
 
 TechniciansByAttribute = () => {
-app.get('/technicians/firstname/:first_name', (req,res)=>{
-    res.json(technicians.filter(tech => tech.first_name.includes(req.params.first_name)))
-})
+    app.get('/technicians/firstname/:first_name', (req,res)=>{
+        res.json(technicians.filter(tech => tech.first_name.includes(req.params.first_name)));
+    })
 }
 TechniciansByAttribute();
 
 // Delete technicians by ID
 
 deleteTechnicianById = () => {
-app.delete('/technicians/:id', (req,res) => {
-    res.json({msg: `Technician ${req.params.id} deleted`, technicians: technicians.filter(tech => tech.id !== parseInt(req.params.id))})
-})
-
+    app.delete('/technicians/:id', (req,res) => {
+        const techniciansLeft = technicians.filter(tech => tech.id !== parseInt(req.params.id));
+        fs.writeFile('data/technicians.json',JSON.stringify(techniciansLeft),err=>{
+            if(err){
+                console.log(err);
+            }   else{
+                    res.json({msg: `Technician ${req.params.id} deleted`, Technicians: techniciansLeft });
+                }
+        });
+        
+    });
 }
 deleteTechnicianById();
 
