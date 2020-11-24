dc059c825592eb96ceb794b0fe532a031ede77eb
Ignacio Aristo <ignacioaristo@gmail.com>
2020-11-24 18:03:47 -0300

    minimal changes

--- controllers/technicians.js
+++ controllers/technicians.js
@@ -24,14 +24,14 @@ app.get('/getTechniciansByName', (req,res) => {
 // Delete technicians by ID
 
 app.get('/deleteTechnicianById', (req,res) => {
-	const techniciansLeft = technicians.filter(tech => tech.id !== parseInt(req.query.id));
-	fs.writeFile('data/technicians.json',JSON.stringify(techniciansLeft),err=>{
-		if(err){
-			console.log(err);
-		}	else{
-				res.json({msg: `Technician ${req.params.id} deleted`, Technicians: techniciansLeft });
-			}
-	});
+  const techniciansLeft = technicians.filter(tech => tech.id !== parseInt(req.query.id));
+  fs.writeFile('data/technicians.json',JSON.stringify(techniciansLeft),err=>{
+    if(err){
+      console.log(err);
+    }	else{
+        res.json({msg: `Technician ${req.params.id} deleted`, Technicians: techniciansLeft });
+      }
+  });
 });
 
 module.exports = app;
