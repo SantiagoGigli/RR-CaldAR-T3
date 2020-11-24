24a82b2c2d4c9b3c31cae974f1c42f842dbf177a
Mariana Sanchez <marianabsanchez@hotmail.com>
2020-11-24 00:27:10 -0300

    Add spaces and query params

--- controllers/boilers.js
+++ controllers/boilers.js
@@ -1,68 +1,64 @@
-const { json } = require('express');
-const express=require('express')
-const router=express.Router();
-const fs= require('fs');
-
-const boilersData=require('../data/boilers.json');
+const express = require('express');
+const fs = require('fs');
+const router = express.Router();
+const boilers = require('../data/boilers.json');
 
 //Get all boilers
-
-router.get('/boilers',(req,res)=>{
-    res.json(boilersData);
+router.get('/getAllBoilers', (req, res) => {
+  res.json(boilers);
 })
 
 //Get boiler by Id
-router.get('/boilers/:id',(req,res)=>{
-    const found=boilersData.some(boiler=>boiler.id===parseInt(req.params.id));
-
-    if (found){
-        res.json(boilersData.filter(boiler=>boiler.id===parseInt(req.params.id)));
-    }
-    else{
-        res.status(400).json({msg: 'The boiler with id ' + req.params.id + ' does not exist'});
-    }
+router.get('/getBoilerById', (req, res) => {
+  const id = req.query.id;
+  const found = boilers.some(boiler => boiler.id === parseInt(id));
+  if (found){
+    res.json(boilers.filter(boiler => boiler.id === parseInt(id)));
+  }
+  else{
+    res.status(400).json({msg: 'The boiler with id ' + id + ' does not exist'});
+  }
 });
 
 //Get Boiler By Attribute: Type Id
-router.get('/boilers/typeId/:typeId', (req,res)=>{
-    const boilersFound=boilersData.some(boiler=>boiler.typeId===parseInt(req.params.typeId))
-    if(boilersFound){
-        res.json(boilersData.filter(boiler=>boiler.typeId===parseInt(req.params.typeId)));
-    }
-    else{
-        res.status(400).json({msg: 'There are no boilers of the type ' + req.params.typeId });
-    }
+router.get('/getBoilersByType', (req, res) => {
+  const type = req.query.type;
+  const boilersFound = boilers.some(boiler => boiler.typeId === parseInt(type));
+  if(boilersFound){
+    res.json(boilers.filter(boiler => boiler.typeId === parseInt(type)));
+  }
+  else{
+    res.status(400).json({msg: 'There are no boilers of the type ' + typeId});
+  }
 })
 
-//Get Boiler By Attribute: Maintenace Rate
-router.get('/boilers/maintainaceRate/:maintainace_rate', (req,res)=>{
-    const boilersFound=boilersData.some(boiler=>boiler.maintainace_rate===req.params.maintainace_rate)
-    if(boilersFound){
-        res.json(boilersData.filter(boiler=>boiler.maintainace_rate===req.params.maintainace_rate));
-    }
-    else{
-        res.status(400).json({msg: 'There are no boilers with maintainace ' + req.params.maintainace_rate });
-    }
+//Get Boiler By Attribute: Maintainace Rate
+router.get('/getBoilersByMaintainaceRate', (req, res) => {
+  const mRate = req.query.mRate;
+  const boilersFound = boilers.some(boiler => boiler.maintainace_rate === mRate);
+  if(boilersFound){
+    res.json(boilers.filter(boiler => boiler.maintainace_rate === mRate));
+  }
+  else{
+    res.status(400).json({msg: 'There are no boilers with maintainace ' + mRate });
+  }
 })
 
 
 //Delete boiler by Id
-router.get('/boilers/delete/:id',(req,res)=>{
-    const found=boilersData.some(boiler=>boiler.id===parseInt(req.params.id));
-    
-    if (found){
-        const remainingBoilers=boilersData.filter(boiler=>boiler.id!==parseInt(req.params.id));
-        fs.writeFile('data/boilers.json',JSON.stringify(remainingBoilers),err=>{
-            if(err){
-                console.log(err);
-            }
-        });
-        res.json({msg: 'Boiler deleted ', boilers: remainingBoilers});
-    }
-    else{
-        res.status(400).json({msg: 'The boiler with id ' + req.params.id + ' does not exist'});
-    }
+router.get('/deleteBoilerById', (req, res) => {
+  const id = req.query.id;
+  const found = boilers.some(boiler => boiler.id === parseInt(id));  
+  if (found){
+    const remainingBoilers = boilers.filter(boiler => boiler.id !== parseInt(id));
+    fs.writeFile('data/boilers.json', JSON.stringify(remainingBoilers), err=>{
+      if(err) console.log(err);      
+    });
+    res.json({msg: 'Boiler deleted ', boilers: remainingBoilers});
+  }
+  else{
+    res.status(400).json({msg: 'The boiler with id ' + id + ' does not exist'});
+  }
 });
-
 
 module.exports=router;
