c834cbedc864d3a1a7c57d98e1ba2a9dda688487
Augusto <augustoarlt95@gmail.com>
2020-11-24 22:13:46 -0300

    Remove utils

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const abmUtils = require('../utils/ABMUtils');
 const appointmentsData = require('../data/APPOINTMENTS_MOCK_DATA.json');
 
 router.get('/getAllAppointments', (req, res) => {
@@ -8,11 +7,26 @@ router.get('/getAllAppointments', (req, res) => {
 });
 
 router.get('/getById', (req, res) => {
-  abmUtils.getById(req, res, appointmentsData);
+    const requestEntityId = parseInt(req.query.id);
+    const entity = appointmentsData.filter(ent => ent.id === requestEntityId);
+    entity.length ? res.json(entity) : res.status(400).json({msg: `The entity with id ${requestEntityId} doesn't exist`});
 });
 
 router.get('/getByAttribute', (req, res) => {
-  abmUtils.getByAttribute(req, res, appointmentsData);
+  const queryKeys = Object.keys(req.query);
+  const queryAttr = queryKeys.map(key => req.query[key]);
+  const matchingEntities = [];
+  for (let i = 0; i < appointmentsData.length; i++) {
+    const entity = appointmentsData[i];
+    const entityAttrs = queryKeys.map(key => entity[key]);
+    const areArraysEqual =entityAttrs.every((val, index) => val == queryAttr[index])
+    if (queryAttr.length === entityAttrs.length && areArraysEqual) matchingEntities.push(entity);
+  }
+  if (entity) {
+    res.json(matchingEntities);
+  } else {
+    res.status(400).json({msg: `The entity with id ${requestEntityId} doesn't exist`});
+  }
 });
 
 router.get('/deleteById', (req, res) => {
--- controllers/buildings.js
+++ controllers/buildings.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const abmUtils = require('../utils/ABMUtils');
 const buildingsData = require('../data/BULDINGS_MOCK_DATA.json');
 
 router.get('/getAllBuildings', (req, res) => {
@@ -8,15 +7,44 @@ router.get('/getAllBuildings', (req, res) => {
 });
 
 router.get('/getById', (req, res) => {
-  abmUtils.getById(req, res, buildingsData);
+  const requestEntityId = parseInt(req.query.id);
+  const entity = buildingsData.some(ent => ent.id === requestEntityId);
+  if (entity) {
+    res.json(entity);
+  } else {
+    res.status(400).json({msg: `The entity with id ${requestEntityId} doesn't exist`});
+  }
 });
 
 router.get('/getByAttribute', (req, res) => {
-  abmUtils.getByAttribute(req, res, buildingsData);
+  const queryKeys = Object.keys(req.query);
+  const queryAttr = queryKeys.map(key => req.query[key]);
+  const matchingEntities = [];
+  for (let i = 0; i < buildingsData.length; i++) {
+    const entity = buildingsData[i];
+    const entityAttrs = queryKeys.map(key => entity[key]);
+    const areArraysEqual =entityAttrs.every((val, index) => val == queryAttr[index])
+    if (queryAttr.length === entityAttrs.length && areArraysEqual) matchingEntities.push(entity);
+  }
+  if (entity) {
+    res.json(matchingEntities);
+  } else {
+    res.status(400).json({msg: `The entity with id ${requestEntityId} doesn't exist`});
+  }
 });
 
 router.get('/deleteById', (req, res) => {
-  abmUtils.deleteById(req, res, buildingsData, '/../data/BULDINGS_MOCK_DATA.json');
+  const requestEntityId = parseInt(req.query.id);
+  const entity = buildingsData.some(ent => ent.id === requestEntityId);
+  if (entity) {
+    const newData = buildingsData.filter(ent => ent.id !== requestEntityId);
+    fs.writeFile(__dirname + mockedDataFilePath, JSON.stringify(newData), err => {
+      if (err) console.log(err);
+    });
+    res.json({msg: 'Entity deleted', result: newData});
+  } else {
+    res.status(400).json({msg: `The entity with id ${requestEntityId} doesn't exist`});
+  }
 });
 
 module.exports = router;
