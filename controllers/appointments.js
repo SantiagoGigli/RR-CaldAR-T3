484794e769a8170693effb9ed5d09d460ebe94fa
Augusto <augustoarlt95@gmail.com>
2020-11-24 22:30:22 -0300

    fix appointments endpoints

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const router = express.Router();
+const fs = require('fs');
 const appointmentsData = require('../data/APPOINTMENTS_MOCK_DATA.json');
+const router = express.Router();
 
 router.get('/getAllAppointments', (req, res) => {
   res.json(appointmentsData);
@@ -8,8 +9,12 @@ router.get('/getAllAppointments', (req, res) => {
 
 router.get('/getById', (req, res) => {
     const requestEntityId = parseInt(req.query.id);
-    const entity = appointmentsData.filter(ent => ent.id === requestEntityId);
-    entity.length ? res.json(entity) : res.status(400).json({msg: `The entity with id ${requestEntityId} doesn't exist`});
+    const entity = appointmentsData.find(ent => ent.id === requestEntityId);
+    if (entity) {
+      res.json(entity);
+    } else {
+      res.status(400).json({msg: `The entity with id ${requestEntityId} doesn't exist`});
+    }
 });
 
 router.get('/getByAttribute', (req, res) => {
@@ -19,18 +24,28 @@ router.get('/getByAttribute', (req, res) => {
   for (let i = 0; i < appointmentsData.length; i++) {
     const entity = appointmentsData[i];
     const entityAttrs = queryKeys.map(key => entity[key]);
-    const areArraysEqual =entityAttrs.every((val, index) => val == queryAttr[index])
+    const areArraysEqual = entityAttrs.every((val, index) => val == queryAttr[index])
     if (queryAttr.length === entityAttrs.length && areArraysEqual) matchingEntities.push(entity);
   }
-  if (entity) {
+  if (matchingEntities.length) {
     res.json(matchingEntities);
   } else {
-    res.status(400).json({msg: `The entity with id ${requestEntityId} doesn't exist`});
+    res.status(400).json({msg: `No results`});
   }
 });
 
 router.get('/deleteById', (req, res) => {
-  abmUtils.deleteById(req, res, appointmentsData, '/../data/APPOINTMENTS_MOCK_DATA.json');
+  const requestEntityId = parseInt(req.query.id);
+  const entity = appointmentsData.some(ent => ent.id === requestEntityId);
+  if (entity) {
+    const newData = appointmentsData.filter(ent => ent.id !== requestEntityId);
+    fs.writeFile(__dirname + '/../data/APPOINTMENTS_MOCK_DATA.json', JSON.stringify(newData), err => {
+      if (err) console.log(err);
+    });
+    res.json({msg: 'Entity deleted', result: newData});
+  } else {
+    res.status(400).json({msg: `The entity with id ${requestEntityId} doesn't exist`});
+  }
 });
 
 module.exports = router;
