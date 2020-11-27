07082eb657cd25d2e6bc626042a7667b171082a4
Ignacio Aristo <ignacioaristo@gmail.com>
2020-11-26 21:09:12 -0300

    unified codes

--- controllers/technicians.js
+++ controllers/technicians.js
@@ -1,43 +1,43 @@
 const express = require('express');
 const fs = require('fs');
 const technicians = require('../data/technicians.json');
-const app = express();
+const router = express.Router();
 
 // Get all the technicians
-app.get('/getTechniciansAll', (req,res) => {
+router.get('/getAllTechnicians', (req, res) => {
   res.json(technicians);
 });
 
 // Get technicians by ID
-app.get('/getTechnicianById', (req,res) => {
+router.get('/getTechnicianById', (req, res) => {
   let found = technicians.some(tech => tech.id === parseInt(req.query.id));
   if (found){
     res.json(technicians.find(tech => tech.id === parseInt(req.query.id)));
-  } else {
+  } else{
     res.status(404).json({msg: `Technician with ID ${req.query.id} doens't exist`});
   } 
 });
 
 // Get technicians by attribute (Name)
-app.get('/getTechniciansByName', (req,res) => {
+router.get('/getTechniciansByName', (req, res) => {
   res.json(technicians.filter(tech => tech.first_name.includes(req.query.first_name)));
 });
 
 // Delete technicians by ID
-app.get('/deleteTechnicianById', (req,res) => {
+router.get('/deleteTechnicianById', (req, res) => {
   let found = technicians.some(tech => tech.id === parseInt(req.query.id));
   const techniciansLeft = technicians.filter(tech => tech.id !== parseInt(req.query.id));
   if (found){
     fs.writeFile('data/technicians.json', JSON.stringify(techniciansLeft), err => {
       if (err){
         console.log(err);
-      } else {
+      } else{
         res.json({msg: `technician ${req.query.id} deleted`, Technicians: techniciansLeft});
       }
     });
-  } else {
+  } else{
     res.status(404).json({msg: `Technician with ID ${req.query.id} doens't exist`});
   }
 });
 
-module.exports = app;
+module.exports = router;
--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const express = require('express');
-const controllersBoilersTypes = require('./controllers/boilers-types');
+const controllerBoilersTypes = require('./controllers/boilersTypes');
 const controllerAppointments = require('./controllers/appointments');
 const controllerBuildings = require('./controllers/buildings');
-const controllersCustomers = require('./controllers/Customers');
-const controllersBoilers = require('./controllers/boilers');
-const controllersTechnicians = require('./controllers/technicians');
+const controllerCustomers = require('./controllers/Customers');
+const controllerBoilers = require('./controllers/boilers');
+const controllerTechnicians = require('./controllers/technicians');
 const app = express();
 const port = 3000;
 
-app.use('/boilers-types', controllersBoilersTypes);
-app.use('/', controllersCustomers);
-app.use('/', controllersBoilers);
-app.use('/api/buildings', controllerBuildings);
-app.use('/api/appointments', controllerAppointments);
-app.use('/', controllersTechnicians);
+app.use('/boilerTypes', controllerBoilersTypes);
+app.use('/customers', controllerCustomers);
+app.use('/boilers', controllerBoilers);
+app.use('/buildings', controllerBuildings);
+app.use('/appointments', controllerAppointments);
+app.use('/technicians', controllerTechnicians);
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
