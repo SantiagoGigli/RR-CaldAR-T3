ee00f2e27420767b4b103f8f6257744bafac002d
Ignacio Aristo <ignacioaristo@gmail.com>
2020-11-28 10:16:35 -0300

    added routes

--- routes/technicians.js
+++ routes/technicians.js
@@ -2,8 +2,19 @@ const technicians = require('../controllers/technicians.js');
 const router = require('express').Router();
 
 
-// // Retrieve all technicians
-// router.get('/', technicians.findAll);
+// // Add a new technician
+// router.get('/add', technicians.findAll);
 
+// // Update a technician
+// router.get('/updateById/:id', technicians.update);
+
+// // Delete a technician by ID
+// router.get('/deleteById/:id', technicians.delete);
+
+// // Get all the technicians
+// router.get('/deleteById', technicians.findAll);
+
+// // Get technician by ID
+// router.get('/deleteById/:id', technicians.findOne);
 
 module.exports = router;
