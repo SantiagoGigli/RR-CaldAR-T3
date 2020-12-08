e15edf78bfb59295b5dbbae128d859ee75ea5fe4
Mariana Sanchez <marianabsanchez@hotmail.com>
2020-12-07 22:51:51 -0300

    Puerto con var de entorno

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-//Emiliano
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const dotenv = require('dotenv').config();
 const db = require('./models');
 const router = require('./routes');
