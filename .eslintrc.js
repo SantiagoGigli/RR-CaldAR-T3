0ffa15b44320abb90bf730e204c0f3fa63794be3
SantiagoGigli <santigigli93@gmail.com>
2020-12-08 15:37:30 -0300

    airbnb eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,14 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-        "no-console": "off",
-        "quotes": [
-            "error",
-            "single"
-        ]
-    }
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "airbnb-base",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    "no-plusplus": "off",
+  },
 };
