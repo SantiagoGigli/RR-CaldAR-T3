19de0f40e19b58470e3bfd7508fc19bd70e5398d
Mariana Sanchez <marianabsanchez@hotmail.com>
2020-12-10 00:35:44 -0300

    Fix details

--- models/index.js
+++ models/index.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const db = {};
 db.mongoose = mongoose;
 db.url = process.env.mongoURL;
-db.url = 'mongodb+srv://IgnacioAristo:radium1234@caldar.ejvrp.mongodb.net/caldar?retryWrites=true&w=majority';
 db.Technicians = require('./technicians.js')(mongoose);
 db.Buildings = require('./buildings.js')(mongoose);
 db.Customers = require('./customers.js')(mongoose);
