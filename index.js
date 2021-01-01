0d22fd5be68f34064ed1c14856dd352a477e199a
Ignacio Aristo <ignacioaristo@gmail.com>
2021-01-01 16:30:50 -0300

    fixed cors

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-// const cors = require('cors');
+const cors = require('cors');
 
-// app.use(cors);
+app.use(cors());
 
 const port = process.env.PORT || 3000;
 require('dotenv').config();
